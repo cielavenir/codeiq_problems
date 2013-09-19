0a4db8918e71165f735474a52b539adf1b88408e
cielavenir <cielartisan@gmail.com>
2013-09-19 12:35:46 +0900

    Pascal: char -> chr

--- q431/helloworld.pas
+++ q431/helloworld.pas
@@ -11,16 +11,16 @@ begin
 	sfive:=szero shl (szero+szero+szero+szero+szero);
 	ssix:=szero shl (szero+szero+szero+szero+szero+szero);
 
-	write(char(ssix+sthree));
-	write(char(ssix+sfive+stwo+szero));
-	write(char(ssix+sfive+sthree+stwo));
-	write(char(ssix+sfive+sthree+stwo));
-	write(char(ssix+sfive+sthree+stwo+sone+szero));
-	write(char(sfive));
-	write(char(ssix+sfour+stwo+sone+szero));
-	write(char(ssix+sfive+sthree+stwo+sone+szero));
-	write(char(ssix+sfive+sfour+sone));
-	write(char(ssix+sfive+sthree+stwo));
-	write(char(ssix+sfive+stwo));
-	writeln(char(sthree+sone));
+	write(chr(ssix+sthree));
+	write(chr(ssix+sfive+stwo+szero));
+	write(chr(ssix+sfive+sthree+stwo));
+	write(chr(ssix+sfive+sthree+stwo));
+	write(chr(ssix+sfive+sthree+stwo+sone+szero));
+	write(chr(sfive));
+	write(chr(ssix+sfour+stwo+sone+szero));
+	write(chr(ssix+sfive+sthree+stwo+sone+szero));
+	write(chr(ssix+sfive+sfour+sone));
+	write(chr(ssix+sfive+sthree+stwo));
+	write(chr(ssix+sfive+stwo));
+	writeln(chr(sthree+sone));
 end.
