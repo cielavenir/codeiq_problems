6223ba2c50a9fc611dc98cd6b87936fd035f8974
cielavenir <cielartisan@gmail.com>
2014-07-31 10:32:09 +0900

    fixed helloworld swift

--- q431/helloworld.swift
+++ q431/helloworld.swift
@@ -1,26 +1,30 @@
 import Foundation
 
-var output = NSFileHandle.fileHandleWithStandardOutput()
-var arr = [true]
-var szero = arr.count
-var sone = szero<<(szero)
-var stwo = szero<<(szero+szero)
-var sthree = szero<<(szero+szero+szero)
-var sfour = szero<<(szero+szero+szero+szero)
-var sfive = szero<<(szero+szero+szero+szero+szero)
-var ssix = szero<<(szero+szero+szero+szero+szero+szero)
+let arr = [true]
+let szero = arr.count
+let sone = szero<<(szero)
+let stwo = szero<<(szero+szero)
+let sthree = szero<<(szero+szero+szero)
+let sfour = szero<<(szero+szero+szero+szero)
+let sfive = szero<<(szero+szero+szero+szero+szero)
+let ssix = szero<<(szero+szero+szero+szero+szero+szero)
 var data = NSMutableData(length:sthree|stwo)
-var bytes = UnsafeArray(start:UnsafePointer<Byte>(data.bytes), length:data.length)
-bytes[szero-szero] = ssix|sthree
-bytes[szero] = ssix|sfive|stwo|szero
-bytes[sone] = ssix|sfive|sthree|stwo
-bytes[sone|szero] = ssix|sfive|sthree|stwo
-bytes[stwo] = ssix|sfive|sthree|stwo|sone|szero
-bytes[stwo|szero] = sfive
-bytes[stwo|sone] = ssix|sfour|stwo|sone|szero
-bytes[stwo|sone|szero] = ssix|sfive|sthree|stwo|sone|szero
-bytes[sthree] = ssix|sfive|sfour|sone
-bytes[sthree|szero] = ssix|sfive|sthree|stwo
-bytes[sthree|sone] = ssix|sfive|stwo
-bytes[sthree|sone|szero] = sthree|sone
-output.writeData(data) //I don't know if println works.
+//var bytes = UnsafeArray(start:UnsafePointer<CChar>(data.bytes), length:data.length)
+var bytes = UnsafePointer<CChar>(data.bytes)
+//must cast to CChar explicitly.
+bytes[szero-szero] = CChar(ssix|sthree)
+bytes[szero] = CChar(ssix|sfive|stwo|szero)
+bytes[sone] = CChar(ssix|sfive|sthree|stwo)
+bytes[sone|szero] = CChar(ssix|sfive|sthree|stwo)
+bytes[stwo] = CChar(ssix|sfive|sthree|stwo|sone|szero)
+bytes[stwo|szero] = CChar(sfive)
+bytes[stwo|sone] = CChar(ssix|sfour|stwo|sone|szero)
+bytes[stwo|sone|szero] = CChar(ssix|sfive|sthree|stwo|sone|szero)
+bytes[sthree] = CChar(ssix|sfive|sfour|sone)
+bytes[sthree|szero] = CChar(ssix|sfive|sthree|stwo)
+bytes[sthree|sone] = CChar(ssix|sfive|stwo)
+bytes[sthree|sone|szero] = CChar(sthree|sone)
+
+//both of these works.
+//NSFileHandle.fileHandleWithStandardOutput().writeData(data)
+//print(NSString(data:data, encoding:NSUTF8StringEncoding))
