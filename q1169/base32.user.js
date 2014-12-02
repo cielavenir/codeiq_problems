810077730e68a1e54596d05b3cff6e8b658ec6e6
cielavenir <cielartisan@gmail.com>
2014-12-02 22:21:32 +0900

    fix

--- q1169/base32.user.js
+++ q1169/base32.user.js
@@ -8,7 +8,7 @@ window.document.onclick =  function(event){
   if (output == null) {
     return;
   }
-  var answer = output.innerHTML.replace(/[\n\r]/g, "");
+  var answer = output.innerHTML.replace(/[\n\r]+$/, "");
   if (answer == "CodeIQ is a service in which you can check your programming skill. There are so many problems such as algorithms, mathematics, data sciences, and even security! The unique point is that (unlike other e-learnings) you can receive feedback comment from human, which will be helpful for your study. So, let's go for it!") {
     console.log("OK");
   } else {
