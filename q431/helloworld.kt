10ff4430fdf00c77d874cc4a60de225de310388b
cielavenir <cielartisan@gmail.com>
2018-12-09 10:43:59 +0900

    update kt for native

--- q431/helloworld.kt
+++ q431/helloworld.kt
@@ -1,24 +1,23 @@
-import java.util.*
-import java.math.*
+import kotlin.math.*
 fun main(args: Array<String>){
-	val szero=Math.log(Math.E).toInt()
-	val sone=szero+szero
+	val sone=E.toInt()
+	val szero=sone/sone
 	val stwo=sone+sone
 	val sthree=stwo+stwo
 	val sfour=sthree+sthree
 	val sfive=sfour+sfour
 	val ssix=sfive+sfive
 
-	print((ssix+sthree).toChar());
-	print((ssix+sfive+stwo+szero).toChar());
-	print((ssix+sfive+sthree+stwo).toChar());
-	print((ssix+sfive+sthree+stwo).toChar());
-	print((ssix+sfive+sthree+stwo+sone+szero).toChar());
-	print((sfive).toChar());
-	print((ssix+sfour+stwo+sone+szero).toChar());
-	print((ssix+sfive+sthree+stwo+sone+szero).toChar());
-	print((ssix+sfive+sfour+sone).toChar());
-	print((ssix+sfive+sthree+stwo).toChar());
-	print((ssix+sfive+stwo).toChar());
-	println();
+	print((ssix+sthree).toChar())
+	print((ssix+sfive+stwo+szero).toChar())
+	print((ssix+sfive+sthree+stwo).toChar())
+	print((ssix+sfive+sthree+stwo).toChar())
+	print((ssix+sfive+sthree+stwo+sone+szero).toChar())
+	print((sfive).toChar())
+	print((ssix+sfour+stwo+sone+szero).toChar())
+	print((ssix+sfive+sthree+stwo+sone+szero).toChar())
+	print((ssix+sfive+sfour+sone).toChar())
+	print((ssix+sfive+sthree+stwo).toChar())
+	print((ssix+sfive+stwo).toChar())
+	println()
 }
